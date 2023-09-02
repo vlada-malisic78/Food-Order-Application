a654dfcbf3b8cc0fb05d4b951b4c0c5d3a31af95
vlada <malisicvlada1978@gmail.com>
2023-09-02 15:49:52 +0200

    Move index.html into public folder

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -1,55 +1,50 @@
-import path from 'path';
+import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-import {
-    Configuration as WebpackConfig,
-    HotModuleReplacementPlugin
-} from 'webpack'
+import { Configuration as WebpackConfig, HotModuleReplacementPlugin } from 'webpack'
 
-import { Configuration as WebpackDevServerConfig } from 'webpack-dev-server';
+import { Configuration as WebpackDevServerConfig } from 'webpack-dev-server'
 
 type Configuration = WebpackConfig & {
-    devServer? : WebpackDevServerConfig
+  devServer?: WebpackDevServerConfig
 }
 
-const config : Configuration =  {
-    mode:'development',
-    output:{
-        publicPath:'/'
-    },
-    entry:'./src/index.tsx',
-    module:{
-        rules:[
-            {
-                test:/\.(ts|js)x?$/i,
-                exclude:/node_modules/,
-                use:{
-                    loader:'babel-loader',
-                    options:{
-                        presets:['@babel/preset-env','@babel/preset-react','@babel/preset-typescript'],
-                    }
-                }
-        
-            }
-        ]
-    },
-    resolve:{
-        extensions:['.tsx','.ts','.js']
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template:'src/index.html'
-        }),
-        new HotModuleReplacementPlugin()
+const config: Configuration = {
+  mode: 'development',
+  output: {
+    publicPath: '/',
+  },
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
     ],
-    devtool:'inline-source-map',
-    devServer: {
-        static:path.join(__dirname,'dist'),
-        historyApiFallback:true,
-        port:4000,
-        open:true,
-        hot:true,
-    },
-   
-} 
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+    new HotModuleReplacementPlugin(),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    historyApiFallback: true,
+    port: 4000,
+    open: true,
+    hot: true,
+  },
+}
 
-export default config;
+export default config
